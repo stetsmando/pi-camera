d5f673f1509150d7a9729512cd07a90beac2074c
stetsmando <stetsmando@gmail.com>
2017-05-31 09:09:19 -0600

    Add simple function

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const execute = require('./lib/exec');
 
 // Class being exported
 exports.PiCamera = function() {
-  
+  execute('ls')
+    .then((msg) => {
+      console.log(msg);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
